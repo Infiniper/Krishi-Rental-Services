75bef4767a1c340a63bfdf56eb138b2d95e12b16
Shashwat <shashwatp108@gmail.com>
2025-03-30 03:31:05 +0530

    booking page started

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -3,13 +3,24 @@ import Home from "./pages/home/home";
 import "./styles/global.css";
 import Search from "./pages/search/search";
 import About from "./pages/about/about";
+import Booking from "./pages/booking/booking";
 
 function App() {
   return(
     <div>
       {/* <Home /> */}
       {/* <Search /> */}
-      <About />
+      {/* <About /> */}
+      <Booking 
+      image = "https://res.cloudinary.com/ddyxqpatx/image/upload/v1742736399/se/avweov9karwcojja3taq.jpg"
+      type = "Tractor"
+      model = "Mahindra Arjun Novo"
+      status =  "Available"
+      owner =  "Vishwajeet Singh"
+      price =  "2999"
+      registration="UP 707 420"
+      discription = "A tractor is an engineering vehicle specifically designed to deliver a high tractive effort (or torque) at slow speeds, for the purposes of hauling a trailer or machinery such as that used in agriculture, mining or construction."
+      />
     </div>
   );
 };
--- web-frontend/src/pages/booking/booking.jsx
+++ web-frontend/src/pages/booking/booking.jsx
@@ -1,21 +1,56 @@
 import React from "react";
 import DummyData from "../search/components/dummy-data";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import styles from "./booking.module.css";
+import Button1 from "../../components/button1/button1";
 
-const Booking = () => {
+
+
+
+const Booking = ({image, type, model, status, owner, price, registration, discription}) => {
 
     return (
-        <div>
-            <h2>Booking Details</h2>
-            {DummyData.map((item, index) => (
-                    <SearchCard
-                        key={index}
-                        image={item.image}
-                        title={item.title}
-                        distance={item.distance}
-                        price={item.price}
-                        available={item.available}
-                    />
-                ))}
+        <div className={styles.container}>
+            <Header />
+            <p className={styles.margin}>margin</p>
+            <h1>Booking Details</h1>
+            <div className={styles.box}>
+            <img src={image} alt="Machinery_Image" />
+            <div className={styles.details}>
+                <div className={styles.types}>
+                    <h2>{type}</h2>
+                    <h3>{status}</h3>
+                </div>
+                <div className={styles.booking}>
+                <h3 className={styles.model}>{model}</h3>
+                <Button1 
+                    label={"Book now"}
+                />
+                </div>
+                <h3 className={styles.price}>₹{price}</h3>
+                <ul className={styles.further}>
+                    <li className={styles.owner}>{owner}</li>
+                    <li>{registration}</li>
+                    <li>{discription}</li>
+                </ul>
+            </div>
+
+            </div>
+            <Footer />
         </div>
     );
 }
+
+/*
+Img
+Machinery type
+Model
+Status
+Owner name
+Price
+Ragistration no
+Description
+*/
+
+export default Booking;
