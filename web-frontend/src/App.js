b35170171b1a3bf710be38cbcec5bdcbd0d1e9e1
Shashwat <shashwatp108@gmail.com>
2025-03-30 01:13:20 +0530

    about page

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./pages/home/home";
 import "./styles/global.css";
 import Search from "./pages/search/search";
+import About from "./pages/about/about";
 
 function App() {
   return(
     <div>
-      <Home />
+      {/* <Home /> */}
       {/* <Search /> */}
+      <About />
     </div>
   );
 };
