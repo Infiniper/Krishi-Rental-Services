0846a1190a8aa43bcdc1202f0a5ae5a6bfbbfec9
Shashwat <shashwatp108@gmail.com>
2025-03-30 18:19:41 +0530

    deleting web-frontend

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -8,10 +8,10 @@ import Booking from "./pages/booking/booking";
 function App() {
   return(
     <div>
-      {/* <Home /> */}
+      <Home />
       {/* <Search /> */}
       {/* <About /> */}
-      <Booking 
+      {/* <Booking 
       image = "https://res.cloudinary.com/ddyxqpatx/image/upload/v1742736399/se/avweov9karwcojja3taq.jpg"
       type = "Tractor"
       model = "Mahindra Arjun Novo"
@@ -20,7 +20,7 @@ function App() {
       price =  "2999"
       registration="UP 707 420"
       discription = "A tractor is an engineering vehicle specifically designed to deliver a high tractive effort (or torque) at slow speeds, for the purposes of hauling a trailer or machinery such as that used in agriculture, mining or construction."
-      />
+      /> */}
     </div>
   );
 };
--- web-frontend/src/pages/search/search.jsx
+++ web-frontend/src/pages/search/search.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 import SearchCard from "../../components/search-card/search-card";
@@ -6,6 +7,31 @@ import DummyData from "./components/dummy-data";
 import styles from "./search.module.css";
 
 const Search = () => {
+
+    // const [data, setData] = useState([]);
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(null);
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await axios.get("http://localhost:5000/api/machinery");  // Adjust the URL to match your backend API endpoint
+    //             setData(response.data);
+    //             setLoading(false);
+    //         } catch (error) {
+    //             console.error("Error fetching data:", error);
+    //             setError("Failed to fetch data");
+    //             setLoading(false);
+    //         }
+    //     };
+
+    //     fetchData();
+    // }, []);
+
+    // if (loading) return <p>Loading...</p>;
+    // if (error) return <p>{error}</p>;
+
+
     return (
         <div>
             <Header />
